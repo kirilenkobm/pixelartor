b40f3aa6b906c028b23139b5bb32a752a3b669e4
Bogdan Kirilenko <kirilenkobm@gmail.com>
2018-07-18 00:03:41 +0300

    upgrade

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import argparse
 from skimage import io
 from skimage import transform as tf
 from skimage import exposure
+from skimage import feature
+from skimage import filters
 import numpy as np
 import sys
 
@@ -27,7 +29,10 @@ def parse_args():
                      help="Increase size of the pixels.")
     app.add_argument("--interlacing", "-i", action="store_true", dest="interlacing",
                      help="Apply interlacing.")
-
+    app.add_argument("--edges_sigma", "-e", type=float, default=5.0,
+                     help="Sigma for canny filter.")
+    app.add_argument("--egdes_blur_sigma", type=float, default=0.5,
+                     help="Gaussian filter sigma.")
     if len(sys.argv) < 2:  # no arguments, show help in this case
         app.print_help()
         sys.exit(0)
@@ -35,12 +40,8 @@ def parse_args():
     return args
 
 
-def main():
-    """Main func."""
-    args = parse_args()
-    # im is an W x H x 3 array of 0..1
-    im = tf.resize(io.imread(args.input_img), (H, W))
-    pic = np.zeros((H, W, 3))  # init empty image with zeros
+def make_layout(args):
+    """Define coordinates of blocks."""
     # define start points / step size
     W_pix = 160 if not args.magnify else 80
     H_pix = 100 if not args.magnify else 50
@@ -50,9 +51,34 @@ def main():
     h_step = h_starts[1] - h_starts[0]
     # color bins
     bins = np.linspace(0.0, 1.0, num=args.colors)
+    return W_pix, H_pix, w_starts, w_step, h_starts, h_step, bins
+
+
+def enhance_edges(im, args):
+    """Paint edges black."""
+    edges = feature.canny(im[:, :, 1], sigma=args.edges_sigma)
+    edges = filters.gaussian(edges, sigma=args.egdes_blur_sigma)
+    edges = edges.astype(float) * 2
+    im[:, :, 0] -= edges
+    im[:, :, 1] -= edges
+    im[:, :, 2] -= edges
+    im[im < 0.0] = 0.0
+    return im
+
+
+def main():
+    """Main func."""
+    args = parse_args()
+    # im is an W x H x 3 array of 0..1
+    im = tf.resize(io.imread(args.input_img), (H, W))
+    pic = np.zeros((H, W, 3))  # init empty image with zeros
+    # image markup
+    W_pix, H_pix, w_starts, w_step, h_starts, h_step, bins = make_layout(args)
     # contrast percentiles
     perc_left, perc_right = np.percentile(im, (args.left_pecrentile, args.right_pecrentile))
     im = exposure.rescale_intensity(im, in_range=(perc_left, perc_right))
+    # find edges
+    im = enhance_edges(im, args)
 
     # main loop \ "pixel" by "pixel"
     for w_startf in w_starts:
